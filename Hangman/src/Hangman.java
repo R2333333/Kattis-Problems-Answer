b9e9836b40eab11acb2df9b8efff9df43a5bcdc2
Roy Xu <roy.xu@carleton.ca>
2019-03-16 18:27:22 -0400

    correction

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Hangman {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        String question = input.nextLine();
+        String ans = input.nextLine();
+        input.close();
+
+        int num = 10;
+
+        for (int i = 0; i < ans.length()-1; ++i){
+            if (question.contains(ans.substring(i,i+1))){
+                question = question.replace(ans.substring(i,i+1),"");
+            }else {
+                num--;
+            }
+
+            if (question.length() == 0){
+                System.out.println("WIN");
+                break;
+            }
+
+            if (num == 0){
+                System.out.println("LOSE");
+                break;
+            }
+        }
+
+//        question = question.replaceAll("N","");
+
+
+    }
 }
